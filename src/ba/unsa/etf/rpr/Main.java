5a63f61b3ff8f9744159ceb2a83ece07e326b0ce
unknown <azametica1@etf.unsa.ba>
2022-10-17 13:12:23 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int x)
+    {
+        int suma = 0;
+        while(x!=0)
+        {
+          suma+=x%10;
+          x/=10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Unesite n: ");
+        int n = in.nextInt();
+
+        for(int i = 1; i<n; i++)
+        {
+            if(i%sumaCifara(i)==0)
+                System.out.println(i);
+        }
+    }
+}
